381b0db56afd779364ce3ff9ebca7c2c6fd17d39
luana lisboa martins <luhlisboa.20@gmail.com>
2022-09-09 19:29:50 -0300

    Exercicio em Nestjs

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Tarefa } from './tarefa/entities/tarefa.entity';
+import { TarefaModule } from './tarefa/modules/tarefa.module';
+
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: `localhost`,
+      port: 3306,
+      username: `root`,
+      password: `root`,
+      database: `db_todo`,
+      entities: [Tarefa],
+      synchronize: true
+    }),
+    TarefaModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
